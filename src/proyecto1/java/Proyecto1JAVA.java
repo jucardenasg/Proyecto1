9fa86c675617950568ecb66e6eea6d79b495a40f
Estudiante <Estudiante@10.203.2.77>
2018-08-13 08:37:30 -0500

    commit inicial

---
+++ src/proyecto1/java/Proyecto1JAVA.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1.java;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Proyecto1JAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 20;
+                
+        if (a== 19){
+            System.out.println("hello");
+        }
+        else{
+            System.err.println("No es 20");
+        }
+ 
+        System.out.println("Hola, Mundo");
+    } 
+}
